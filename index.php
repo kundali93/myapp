105b053542881ff91cc3b0c7f085f1e807e0d327
HP <rishi.kanaujia@mncgroup.com>
2022-10-16 19:16:51 +0530

    RDS connection

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "helo";
 try {
     //$dbhost = $_SERVER["awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com"];
-    $link = new mysqli($_SERVER['awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com'], $_SERVER['admin'], $_SERVER['rajesh123A'], $_SERVER['react_crud'], $_SERVER['3306']);
+    $link = new mysqli($_SERVER['awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com:3306'], $_SERVER['admin'], $_SERVER['rajesh123A'], $_SERVER['react_crud'], $_SERVER['3306']);
     //$dbport = $_SERVER["3306"];
     //$dbname = $_SERVER["react_crud"];
     //$charset = 'utf8' ;
