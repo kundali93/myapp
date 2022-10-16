ce4c8bd19859864e6c98b0f5141c6e8c7734e34d
HP <rishi.kanaujia@mncgroup.com>
2022-10-16 19:05:33 +0530

    RDS connection

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
 echo "helo";
-/***
-$dbhost = $_SERVER["awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com"];
-$dbport = $_SERVER["3306"];
-$dbname = $_SERVER["react_crud"];
-$charset = 'utf8' ;
+try {
+    $dbhost = $_SERVER["awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com"];
+    $dbport = $_SERVER["3306"];
+    $dbname = $_SERVER["react_crud"];
+    $charset = 'utf8' ;
 
-$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-$username = $_SERVER['admin'];
-$password = $_SERVER['rajesh123A'];
+    $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+    $username = $_SERVER['admin'];
+    $password = $_SERVER['rajesh123A'];
 
-$pdo = new PDO($dsn, $username, $password);
-***/
+    $pdo = new PDO($dsn, $username, $password);
+    echo "done";
+}
+
+//catch exception
+catch(Exception $e) {
+  echo 'Message: ' .$e->getMessage();
+}
+
+
 ?>
