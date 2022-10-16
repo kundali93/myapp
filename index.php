e1fa6fea8d6af13a33deb56058f986dc69fb0e6c
HP <rishi.kanaujia@mncgroup.com>
2022-10-16 19:02:13 +0530

    RDS connection

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+echo "helo";
+/***
 $dbhost = $_SERVER["awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com"];
 $dbport = $_SERVER["3306"];
 $dbname = $_SERVER["react_crud"];
@@ -9,4 +11,5 @@ $username = $_SERVER['admin'];
 $password = $_SERVER['rajesh123A'];
 
 $pdo = new PDO($dsn, $username, $password);
+***/
 ?>
