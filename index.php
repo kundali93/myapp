731f4ef3b58c22085a4ad2ea5933d5ceae34b61d
HP <rishi.kanaujia@mncgroup.com>
2022-10-16 19:21:41 +0530

    RDS connection

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 echo "helo";
 try {
-    //$dbhost = $_SERVER["awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com"];
-    $link = new mysqli($_SERVER['awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com:3306'], $_SERVER['admin'], $_SERVER['rajesh123A'], $_SERVER['react_crud'], $_SERVER['3306']);
-    //$dbport = $_SERVER["3306"];
-    //$dbname = $_SERVER["react_crud"];
-    //$charset = 'utf8' ;
+    $dbhost = $_SERVER["awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com:3306"];
+    //$link = new mysqli($_SERVER['awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-t4rcjts3piik.ciedmyepmavr.ap-south-1.rds.amazonaws.com:3306'], $_SERVER['admin'], $_SERVER['rajesh123A'], $_SERVER['react_crud'], $_SERVER['3306']);
+    $dbname = $_SERVER["react_crud"];
+    $charset = 'utf8' ;
 
-    //$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-    //$username = $_SERVER['admin'];
-    //$password = $_SERVER['rajesh123A'];
+    $dsn = "mysql:host={$dbhost};dbname={$dbname};charset={$charset}";
+    $username = $_SERVER['admin'];
+    $password = $_SERVER['rajesh123A'];
 
-    //$pdo = new PDO($dsn, $username, $password);
+    $pdo = new PDO($dsn, $username, $password);
     echo "done";
 }
 
