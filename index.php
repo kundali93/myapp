1e25c2338a8e57144b12cf30f2d188c7bc2c586f
HP <rishi.kanaujia@mncgroup.com>
2022-10-16 22:03:34 +0530

    RDS connection

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php
 echo "helo00";
 try {
-    $dbhost = $_SERVER["127.0.0.1:3306"];
-    //$link = new mysqli($_SERVER['awseb-e-ez8jeiprkj-stack-awsebrdsdatabase-tcz1qn5j8iij.ciedmyepmavr.ap-south-1.rds.amazonaws.com'], $_SERVER['admin'], $_SERVER['rajesh123A'], $_SERVER['user'], $_SERVER['3306']);
-    $dbname = $_SERVER["user"];
+
+    $dbhost = $_SERVER['awseb-e-vjmh8svkrk-stack-awsebrdsdatabase-ckov2yb0cvbk.ciedmyepmavr.ap-south-1.rds.amazonaws.com'];
+    $dbport = $_SERVER['3306'];
+    $dbname = $_SERVER['new_db'];
     $charset = 'utf8' ;
 
-    $dsn = "mysql:host={$dbhost};dbname={$dbname};charset={$charset}";
+    $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
     $username = $_SERVER['admin'];
     $password = $_SERVER['rajesh123A'];
 
     $pdo = new PDO($dsn, $username, $password);
-    echo "done";
+
 }
 
 //catch exception
 catch(Exception $e) {
   echo 'Message: ' .$e->getMessage();
 }
-
-
 ?>
