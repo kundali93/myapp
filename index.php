7ed63cf090344424be8d9554839cfa53823ec3a7
HP <rishi.kanaujia@mncgroup.com>
2022-10-16 17:38:05 +0530

    RDS connection

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-$mysqli = new mysqli("database-2.ciedmyepmavr.ap-south-1.rds.amazonaws.com:3306", "admin", "rajesh123A", "react_crud");
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-}
-
-$res = $mysqli->query("SHOW TABLES [FROM react_crud] [LIKE '%']");
-
-echo $res;
-
+echo "hell0";
 ?>
